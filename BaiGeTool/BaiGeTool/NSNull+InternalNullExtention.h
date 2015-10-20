a09f76bf84af3cb06ea0a1e7fdbc53f3b7bb332c
白奇龙 <18601135365@163.com>
2015-10-20 17:51:56 +0800

    提交

---
+++ BaiGeTool/BaiGeTool/NSNull+InternalNullExtention.h
@@ -0,0 +1,15 @@
+//
+//  NSNull+InternalNullExtention.h
+//  NullExtention
+//  From :晓月
+//  解决服务器返回为nsnull的问题
+//  Created by bai on 15/10/20.
+//  Copyright © 2015年 bai. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSNull (InternalNullExtention)
+
+@end
